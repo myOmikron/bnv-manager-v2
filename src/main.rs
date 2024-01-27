394f24d98d4a182898edc35f45a4d92e4dfec96e
myOmikron <git@omikron.dev>
2024-01-27 23:29:40 +0100

    Initialized project

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+//! # bnv-manager-v2
+//!
+//! The second version of the bnv-manager
+
+#![warn(missing_docs, clippy::expect_used, clippy::unwrap_used)]
+
+#[rorm::rorm_main]
+#[tokio::main]
+async fn main() {
+    println!("Hello, world!");
+}
