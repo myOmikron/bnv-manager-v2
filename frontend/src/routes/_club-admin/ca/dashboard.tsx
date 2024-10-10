fec9273c836b61aa07ec137f80a18a9ef91c0ac3
myOmikron <git@omikron.dev>
2024-10-10 05:48:22 +0200

    Added stats to dashboard

--- frontend/src/routes/_club-admin/ca/dashboard.tsx
+++ frontend/src/routes/_club-admin/ca/dashboard.tsx
@@ -7,6 +7,9 @@ import USER_CONTEXT from "src/context/user";
 import { Heading } from "src/components/base/heading";
 import { Button } from "src/components/base/button";
 import { Api } from "src/api/api";
+import { FullClub } from "src/api/generated";
+import { toast } from "react-toastify";
+import { Text } from "src/components/base/text";
 
 /**
  * The properties for {@link ClubAdminDashboard}
@@ -22,24 +25,36 @@ function ClubAdminDashboard(props: ClubAdminDashboardProps) {
 
     const { user } = React.useContext(USER_CONTEXT);
 
+    const [club, setClub] = React.useState<FullClub>();
+
     /**
      * Refresh the club
      */
     const refreshClub = async () => {
-        const res = await Api.clubAdmin;
+        const res = await Api.clubAdmin.clubs.get();
+
+        res.match(
+            (club) => setClub(club),
+            (err) => toast.error(err.message),
+        );
     };
 
     useEffect(() => {
         refreshClub().then();
     }, []);
 
+    if (!club) {
+        return <Text>{t("label.loading")}</Text>;
+    }
+
     return (
         <div className={"flex flex-col gap-12"}>
             <Heading>{tD("heading.welcome", { user: user.display_name })}</Heading>
 
             <div className={"grid grid-cols-1 gap-6 lg:grid-cols-3"}>
-                <Stats label={tD("label.user-count")} value={""} />
-                <Stats label={tD("label.website-count")} value={""} />
+                <Stats label={tD("label.user-count")} value={club.user_count} />
+                <Stats label={tD("label.website-count")} value={club.website_count} />
+                <Stats label={tD("label.mail-domain")} value={club.domain} />
             </div>
 
             <div className={"mt-6 grid grid-cols-1 gap-6 lg:grid-cols-3"}>
