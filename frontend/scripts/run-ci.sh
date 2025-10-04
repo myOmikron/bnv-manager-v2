6d11f93535c88daf91ea9d2a8b44ab0d3a4d4222
myOmikron <git@omikron.dev>
2025-10-05 01:10:09 +0200

    Fixed error codes of grep

--- frontend/scripts/run-ci.sh
+++ frontend/scripts/run-ci.sh
@@ -10,8 +10,8 @@ prettier --check src/
 eslint 'src/**/*.{ts,tsx}' --report-unused-disable-directives --max-warnings 0
 
 # Check for missing translations
-missing=$(grep -Er "\"(\w|-)+\.(\w|-)+\"" public/locales/)
-count=$(echo -n "$missing" | grep -c '^')
+missing=$(grep -Er "\"(\w|-)+\.(\w|-)+\"" public/locales/ || true)
+count=$(echo -n "$missing" | grep -c '^' || true)
 
 if [ "$count" -gt 0 ]; then
   echo "$missing"
