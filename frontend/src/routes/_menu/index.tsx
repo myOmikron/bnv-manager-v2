bca0adb83e76444138aa8afce813eaa9b8e4580d
myOmikron <git@omikron.dev>
2025-10-10 02:39:36 +0200

    Added route for redirecting the user to the correct page based on role

--- frontend/src/routes/_menu/index.tsx
+++ frontend/src/routes/_menu/index.tsx
@@ -1,17 +1,27 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, Navigate } from "@tanstack/react-router";
+import ACCOUNT_CONTEXT from "src/context/account";
+import React from "react";
 
 /**
- * Props for {@link Dashboard}
+ * Props for {@link Index}
  */
-export type DashboardProps = {};
+export type IndexProps = {};
 
 /**
- * Dashboard for all users
+ * Index route
  */
-function Dashboard(props: DashboardProps) {
-    return <div>Hello "/"!</div>;
+export default function Index(props: IndexProps) {
+    const ctx = React.useContext(ACCOUNT_CONTEXT);
+
+    if (ctx.account.role.type === "SuperAdmin") {
+        return <Navigate to={"/a/clubs"} />;
+    } else if (ctx.account.role.type === "ClubAdmin") {
+        return <Navigate to={"/ca/$clubId/dashboard"} params={{ clubId: ctx.account.role.club }} />;
+    } else if (ctx.account.role.type === "ClubMember") {
+        return <Navigate to={"/m/dashboard"} />;
+    }
 }
 
 export const Route = createFileRoute("/_menu/")({
-    component: Dashboard,
+    component: Index,
 });
