8ec7884fee884323abcea7006527d6f9af079f90
myOmikron <git@omikron.dev>
2025-10-11 15:35:53 +0200

    Added export as json and csv

--- frontend/src/routes/_menu/ca/$clubId/_club/members.tsx
+++ frontend/src/routes/_menu/ca/$clubId/_club/members.tsx
@@ -5,6 +5,22 @@ import { Api } from "src/api/api";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "src/components/base/table";
 import TablePagination from "src/components/table-pagination";
 import { Text } from "src/components/base/text";
+import {
+    Dropdown,
+    DropdownButton,
+    DropdownHeading,
+    DropdownItem,
+    DropdownLabel,
+    DropdownMenu,
+    DropdownSection,
+} from "src/components/base/dropdown";
+import { ArrowDownTrayIcon, EllipsisVerticalIcon, TrashIcon } from "@heroicons/react/20/solid";
+import { Button } from "src/components/base/button";
+import { Field, FieldGroup, Fieldset, Label } from "src/components/base/fieldset";
+import { Input } from "src/components/base/input";
+import { downloadFile } from "src/utils/downloader";
+import Form from "src/components/base/form";
+import { useForm } from "@tanstack/react-form";
 
 /**
  * The properties for {@link ClubMembers}
@@ -16,20 +32,110 @@ export type ClubMembersProps = {};
  */
 export default function ClubMembers(props: ClubMembersProps) {
     const [t] = useTranslation("ca-club-view");
+    const [tg] = useTranslation();
 
     const params = Route.useParams();
     const data = Route.useLoaderData();
     const search = Route.useSearch();
+    const navigate = Route.useNavigate();
+
+    const form = useForm({
+        defaultValues: {
+            search: search.search,
+        },
+    });
 
     return (
         <div className={"flex flex-col gap-6"}>
             {data.total > 0 ? (
                 <>
-                    <Table>
+                    <div className={"flex justify-between"}>
+                        <Form onSubmit={form.handleSubmit}>
+                            <Fieldset>
+                                <FieldGroup>
+                                    <form.Field
+                                        name={"search"}
+                                        validators={{
+                                            onChangeAsync: async ({ fieldApi }) => {
+                                                await navigate({
+                                                    to: "/ca/$clubId/members",
+                                                    search: {
+                                                        page: 1,
+                                                        search: fieldApi.state.value,
+                                                    },
+                                                });
+                                            },
+                                            onChangeAsyncDebounceMs: 500,
+                                        }}
+                                    >
+                                        {(fieldApi) => (
+                                            <Field>
+                                                <Label>{t("label.search")}</Label>
+                                                <Input
+                                                    required={true}
+                                                    value={fieldApi.state.value}
+                                                    onChange={(e) => fieldApi.handleChange(e.target.value)}
+                                                />
+                                            </Field>
+                                        )}
+                                    </form.Field>
+                                </FieldGroup>
+                            </Fieldset>
+                        </Form>
+
+                        <div className={"flex gap-3"}>
+                            <Button
+                                className={"h-fit w-fit items-center self-end"}
+                                outline={true}
+                                onClick={async () => {
+                                    const page = await Api.clubAdmins.club.getMembers({
+                                        club_uuid: params.clubId,
+                                        search: search.search,
+                                        limit: 100_000,
+                                        offset: 0,
+                                    });
+                                    downloadFile("members.json", JSON.stringify(page.items));
+                                }}
+                            >
+                                <ArrowDownTrayIcon />
+                                <span>{t("label.download-as-json")}</span>
+                            </Button>
+                            <Button
+                                className={"h-fit w-fit items-center self-end"}
+                                outline={true}
+                                onClick={async () => {
+                                    const page = await Api.clubAdmins.club.getMembers({
+                                        club_uuid: params.clubId,
+                                        search: search.search,
+                                        limit: 100_000,
+                                        offset: 0,
+                                    });
+                                    downloadFile(
+                                        "members.csv",
+                                        "Username,Display name,Email\n" +
+                                            page.items
+                                                .map(
+                                                    (member) =>
+                                                        `${member.username},${member.display_name},${member.email}`,
+                                                )
+                                                .join("\n") +
+                                            "\n",
+                                    );
+                                }}
+                            >
+                                <ArrowDownTrayIcon />
+                                <span>{t("label.download-as-csv")}</span>
+                            </Button>
+                        </div>
+                    </div>
+                    <Table dense={true}>
                         <TableHead>
                             <TableRow>
                                 <TableHeader>{t("label.username")}</TableHeader>
                                 <TableHeader>{t("label.display-name")}</TableHeader>
+                                <TableHeader className={"w-0"}>
+                                    <span className={"sr-only"}>{tg("accessibility.actions")}</span>
+                                </TableHeader>
                             </TableRow>
                         </TableHead>
                         <TableBody>
@@ -37,6 +143,22 @@ export default function ClubMembers(props: ClubMembersProps) {
                                 <TableRow key={item.uuid}>
                                     <TableCell>{item.username}</TableCell>
                                     <TableCell>{item.display_name}</TableCell>
+                                    <TableCell>
+                                        <Dropdown>
+                                            <DropdownButton plain={true}>
+                                                <EllipsisVerticalIcon />
+                                            </DropdownButton>
+                                            <DropdownMenu anchor={"bottom end"}>
+                                                <DropdownSection>
+                                                    <DropdownHeading>{tg("heading.danger-zone")}</DropdownHeading>
+                                                    <DropdownItem>
+                                                        <TrashIcon />
+                                                        <DropdownLabel>{t("label.delete-member")}</DropdownLabel>
+                                                    </DropdownItem>
+                                                </DropdownSection>
+                                            </DropdownMenu>
+                                        </Dropdown>
+                                    </TableCell>
                                 </TableRow>
                             ))}
                         </TableBody>
@@ -65,7 +187,7 @@ type SearchParams = {
     /** Current page that should be displayed */
     page: number;
     /** Search for a user */
-    search?: string;
+    search: string;
 };
 
 export const Route = createFileRoute("/_menu/ca/$clubId/_club/members")({
@@ -75,7 +197,7 @@ export const Route = createFileRoute("/_menu/ca/$clubId/_club/members")({
 
         return {
             page: page <= 0 ? 1 : page,
-            search: search?.search as string | undefined,
+            search: search?.search as string | "",
         };
     },
     loaderDeps: ({ search: { page, search } }) => ({ page, search }),
