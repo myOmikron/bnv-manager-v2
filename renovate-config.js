e1b130585276c519ea5a6f30af65c458b5c1383c
myOmikron <git@omikron.dev>
2026-04-12 17:51:25 +0200

    Renovate config

--- renovate-config.js
+++ renovate-config.js
@@ -3,10 +3,8 @@ module.exports = {
   endpoint: "https://api.github.com/",
   token: process.env.RENOVATE_TOKEN,
   gitAuthor: "Renovate Bot <renovate@omikron.dev>",
-  gitPrivateKey: process.env.RENOVATE_GPG_KEY,
   repositories: ["myOmikron/bnv-manager-v2"],
   executionTimeout: 15,
   repositoryCache: "enabled",
   persistRepoData: true,
-  logLevel: process.env.LOG_LEVEL || "info",
 };
