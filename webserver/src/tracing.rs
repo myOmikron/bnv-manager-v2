5d9e5aef4ae9fe6e1b302d6930ba7b57e51603d7
myOmikron <git@omikron.dev>
2026-06-01 10:44:13 +0200

    Fix tracing upgrade

--- webserver/src/tracing.rs
+++ webserver/src/tracing.rs
@@ -1,11 +1,11 @@
 //! Utilities for configuring tracing
 
 use opentelemetry::trace::TracerProvider;
+use opentelemetry_otlp::ExporterBuildError;
 use opentelemetry_otlp::SpanExporter;
 use opentelemetry_otlp::WithExportConfig;
 use opentelemetry_sdk::Resource;
 use opentelemetry_sdk::trace::SdkTracerProvider;
-use opentelemetry_sdk::trace::TraceError;
 use tracing::Subscriber;
 use tracing_subscriber::Layer;
 use tracing_subscriber::registry::LookupSpan;
@@ -17,12 +17,11 @@ use crate::config::OTEL_EXPORTER_OTLP_ENDPOINT;
 /// The layer is not configurable yet and only suited for development.
 /// It should simply work out of the box with a local jaeger instance.
 pub fn opentelemetry_layer<S: Subscriber + for<'span> LookupSpan<'span>>()
--> Result<impl Layer<S>, TraceError> {
+-> Result<impl Layer<S>, ExporterBuildError> {
     let exporter = SpanExporter::builder()
         .with_tonic()
         .with_endpoint(OTEL_EXPORTER_OTLP_ENDPOINT.clone())
-        .build()
-        .map_err(|e| TraceError::Other(e.into()))?;
+        .build()?;
 
     let resource = Resource::builder().with_service_name("bnv-manager").build();
 
